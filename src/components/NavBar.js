51c5b25f576c1ba3b387c08eb154437e85f3446d
David <david.ndiba@student.moringaschool.com>
2023-07-21 19:12:10 +0300

    raect-hooks-lists and keys

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,18 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
-}
+  return (
+    <nav>
+     {links.map((link, index) => (
+        // Using the map function to create an <a> tag for each link
+        // The 'href' attribute will be '#home', '#about', or '#contact' based on the link
+        // The 'key' prop will be a unique value based on the index of the array
+        <a key={index} href={`#${link}`}>
+          {link}
+        </a>
+      ))}
+    </nav>
+  );
+};
 
 export default NavBar;
